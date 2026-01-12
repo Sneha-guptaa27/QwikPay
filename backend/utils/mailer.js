39fe2f6e9c8598e83de614413b911ab6f52f65e3
Sneha-guptaa27 <sneha207gupta@gmail.com>
2026-01-12 13:46:56 +0530

    Fix gitignore and mailer

--- backend/utils/mailer.js
+++ backend/utils/mailer.js
@@ -1,11 +1,10 @@
-// utils/mailer.js
 const { Resend } = require("resend");
 
 const resend = new Resend(process.env.RESEND_API_KEY);
 
 async function sendMail(to, subject, text) {
   return resend.emails.send({
-    from: "QwikPay <onboarding@resend.dev>", // works without domain setup
+    from: "QwikPay <onboarding@resend.dev>",
     to,
     subject,
     text,
